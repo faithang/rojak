54b6afd8984e4a133c62017f2844261ba136debe
Faith Ang <faith_ang@tech.gov.sg>
2025-12-10 10:51:35 +0800

    data: updated puzzles based on playtesting feedback

--- src/data/cardsData.ts
+++ src/data/cardsData.ts
@@ -1,15 +1,13 @@
 export const cardsData = `Front,Back
-SG founding father + first line of a Backstreet Boys' song,Lee Kuan Yew are my fire
+SG founding father + first line of a Backstreet Boys' song,Lee Kuan Yew are my fire
 Iconic drink-dispensing vehicle + Famous painter who cut his ear off, Milo Van Gogh
 Leader of the Opposition + Country that Americans think is in China,Pritam Singhapore
 Event that occurs in August each year + X-ray doctor,National Day Paradiologist
 "Senator I'm Singaporean!" + What a mum tells a toddler at mealtimes,Shou Zi Chew your food!
 Jamus' catchphrase in GE2020 + No. 1 cause of death in SG,Warms the cockles of my heart disease
-First line of a CNY song + English translation of meimei,Dong dong dong chi-younger sister
 First line of Kit Chan's NDP song + Someone who splashes paint on your door,Whenever I am feeling loan shark
 Boy who never grows up + Expressway in SG,Peter Pan Island Expressway
 First HDB town + Type of singing found in the Swiss Alps,Toa Payodelling
-Telco + Something that's orbiting Earth,Starhubble Space Telescope
 Famous female pop artist + SAF Armour tagline,Taylor Swift and Decisive
 2010s male tween sensation + Spanish non-halal meat,Justin Bieberico Pork
 Spiderman actor + Where many expats live,Tom Holland Village
@@ -20,10 +18,21 @@ Local fried chicken dish + A day the British celebrates with fireworks,Har cheon
 Dimsum dish + Powerhouse of the cell,Siew Mai-tochondria
 Voyeuristic NUS student + Cat & mouse cartoon,Peeping Tom & Jerry
 SG founding father + His reason for matchmaking college graduates,Lee Kuan Yew-genics
-An ex-US president + Task Force Man,Ronald ReaGan Kim Yong
 Popular ski resort in Hokkaido + What brought Sir Stamford Raffles to Singapore,Nisekolonialism
 Girl with a diary + Mary Shelley's magnum opus,Anne Frankenstein
 Local girls who exclusively date angmohs + 2010s KPop band,Sarong Party Girls Generation
 Chocolate factory owner + Classic breakfast item,Willy Wonkaya toast
-What Heng Swee Kiat had in GE2020 + Emergency Contraceptive,East Coast Plan B
+Heng Swee Kiat's campaign strategy for GE2020 + Emergency Contraceptive,East Coast Plan B
+Chao Recruit's haircut + Japanese octopus balls, Botakoyaki
+Drink stall command for 'less sweet' + Lady Gaga & Bruno Mars' hit song, Siew Dai with a Smile
+Highest point in SG + Former first lady & presidential candididate, Bukit Timah Hillary Clinton
+Former Speaker of Parliament + Classic cocktail, Tan Chuan Jin and Tonic
+Former PM of SG + Common greeting when meeting an old friend, Lee Hsien Loong time no see
+Former Transport Minister charged in court + Classic fairy tale opener, S. Iswarance upon a time
+SG's favourite Japanese clothing brand + Former WP chief, UniqLow Thia Kiang
+Minister for National Development + Jeans brand, Desmond Leevi's
+Real estate developer + What US is colloquially known as 🦅, CapitaLand of the Free
+Ruling party in SG + Trendy song about pens & fruits, PAPPAP
+Fried banana snack + Prince who found(ed) Singapura, Goreng Pisang Nila Utama
+Classic local milky tea + Singer of "Never Gonna Give You Up", Teh TaRick Astley
 `;
